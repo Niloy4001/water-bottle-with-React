7f05d6e69268b1ab7fa7e5876772b30b3c4178b2
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-10-24 12:34:15 +0600

    file set up

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Headers/Header'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+      <Header></Header>
+      
+    </>
+  )
+}
+
+export default App
